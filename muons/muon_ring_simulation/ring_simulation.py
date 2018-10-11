9450138fdaa662bec81af0d44cbffe279688d2b5
Laurits7 <laurits.tani@gmail.com>
2018-10-11 15:44:21 +0200

    First commit for simulating muon event

---
+++ muons/muon_ring_simulation/ring_simulation.py
@@ -0,0 +1,215 @@
+import numpy as np
+import fact
+import scipy
+import photon_stream as ps
+import single_photon_extractor as spe
+
+
+def pol2cart(r, azimuth, inclination):
+    x = r * np.cos(azimuth) * np.sin(inclination)
+    y = r * np.sin(azimuth) * np.sin(inclination)
+    z = r * np.cos(inclination)
+    return np.array([float(x), float(y), float(z)])
+
+
+def casual_trajectory(support_vector, direction_vector, muon_travel_dist=1000):
+    direction_casual = -direction_vector
+    support_casual = muon_travel_dist*direction_vector + support_vector
+    return support_casual, direction_casual
+
+
+def position_on_ray(support, direction, alpha):
+    return support + alpha * direction
+
+
+def get_d_alpha(ch_rate):
+    ''' ch_rate ->  Cherenkov_photon production rate [1/m]
+    d_alpha [m]'''
+    d_alpha = -np.log(np.random.uniform())/ch_rate
+    return d_alpha
+
+
+def emit_photons(muon_support, muon_direction, opening_angle, ch_rate):
+    muon_support = np.array(muon_support)
+    muon_direction = np.array(muon_direction)
+    path_length = 0
+    photon_emission_pos = []
+    photon_directions = []
+    u, v = get_u_v(muon_direction)
+    while True:
+        d_alpha = get_d_alpha(ch_rate)
+        path_length += d_alpha
+        ch_emission_position = position_on_ray(
+            muon_support,
+            muon_direction,
+            path_length
+            )
+        if ch_emission_position[2] <= 0:
+            break
+        photon_emission_pos.append(ch_emission_position)
+        direction_of_photon = draw_direction_of_cherenkov_photon(
+            opening_angle,
+            u,
+            v,
+            muon_direction)
+        photon_directions.append(direction_of_photon)
+    return np.array(photon_emission_pos), np.array(photon_directions)
+
+
+def get_u_v(direction):
+    dx, dy, dz = direction[0], direction[1], direction[2]
+    a = np.sqrt(1 / (1+((dx/dz)**2)))
+    b = np.sqrt(1 - a**2)
+    u = (a, 0, b)
+    v = np.cross(np.array(u), np.array(direction))
+    return np.array(u), v
+
+
+def draw_direction_of_cherenkov_photon(opening_angle, u, v, muon_direction):
+    azimuth = np.random.uniform(
+        low=0,
+        high=2*np.pi)
+    position_on_ring = np.cos(azimuth)*u + np.sin(azimuth)*v
+    dist_on_mu_path = 1 / np.tan(opening_angle)
+    cherenkov_ph_dir = dist_on_mu_path*muon_direction + position_on_ring
+    return cherenkov_ph_dir/np.linalg.norm(cherenkov_ph_dir)
+
+
+def draw_ch_photon_on_ground(
+    photon_emission_positions,
+    photon_directions
+):
+    t = -photon_emission_positions[:, 2] / photon_directions[:, 2]
+    xys = np.multiply(t[:, np.newaxis], photon_directions)
+    xys += photon_emission_positions
+    return xys
+
+
+def mask_for_photons_in_aperture_radius(
+        intersection_point_on_xy_plane,
+        aperture_radius
+):
+    distances_squared = (
+        intersection_point_on_xy_plane[:, 0]**2
+        + intersection_point_on_xy_plane[:, 1]**2)
+    return distances_squared <= aperture_radius**2
+
+
+def perfect_imaging(
+        photon_emission_positions,
+        photon_directions,
+        aperture_radius=3
+):
+    intersection_points = draw_ch_photon_on_ground(
+        photon_emission_positions,
+        photon_directions
+    )
+    inside_aperture = mask_for_photons_in_aperture_radius(
+        intersection_points,
+        aperture_radius
+    )
+    return photon_directions[inside_aperture, 0:2]
+
+
+def create_fact_pixel_tree():
+    x_pos_mm, y_pos_mm = fact.instrument.get_pixel_coords()
+    x_angle = np.arctan(x_pos_mm/fact.instrument.constants.FOCAL_LENGTH_MM)
+    y_angle = np.arctan(y_pos_mm/fact.instrument.constants.FOCAL_LENGTH_MM)
+    return scipy.spatial.KDTree(np.c_[x_angle, y_angle])
+
+
+def assign_to_pixels(
+    photons_cx_cy,
+    pixel_position_tree,
+    pixel_radius=np.deg2rad(fact.instrument.camera.FOV_PER_PIXEL_DEG)/2
+):
+    distances, pixel_CHIDs = pixel_position_tree.query(
+        photons_cx_cy)
+    inside_field_of_view = distances <= pixel_radius
+    return inside_field_of_view, pixel_CHIDs
+
+
+def arrival_times_for_cherenkov_photons_from_muon(
+    number_photons,
+    arrival_time_std
+):
+    return np.random.normal(
+        loc=0.0,
+        scale=arrival_time_std,
+        size=number_photons)
+
+
+def create_raw_photon_stream(pixel_CHIDs, arrival_times, nsb_rate_per_pixel):
+    arrival_slices = np.round(
+        arrival_times/ps.io.magic_constants.TIME_SLICE_DURATION_S).astype(
+            np.int)
+    lol = spe.air_shower_classification.air_shower_classification.generate_nsb(
+        nsb_rate_per_pixel
+    )
+    for i in range(len(lol)):
+        lol[i] = list(lol[i])
+    for photon, chid in enumerate(pixel_CHIDs):
+        lol[chid].append(arrival_slices[photon])
+    return ps.representations.list_of_lists_to_raw_phs(lol)
+
+
+def simulate_response(
+    muon_support,
+    muon_direction,
+    opening_angle,
+    nsb_rate_per_pixel=35e6,
+    event_id=0,
+    arrival_time_std=500e-12,
+    ch_rate=3,
+    fact_aperture_radius=3.93/2
+):
+    """
+    fact_aperture_radius:
+    @article{ahnen2016bokeh,
+      title={Bokeh mirror alignment for Cherenkov telescopes},
+      author={Ahnen, Max L and Baack,
+      D and Balbo, M and Bergmann,
+      M and Biland, Adrian and Blank,
+      M and Bretz, T and Bruegge,
+      KA and Buss, J and Domke, M and others},
+      journal={Astroparticle Physics},
+      volume={82},
+      pages={1--9},
+      year={2016},
+      publisher={Elsevier}
+    }
+    """
+    ch_sup, ch_dir = emit_photons(
+        muon_support,
+        muon_direction,
+        opening_angle,
+        ch_rate)
+    photons_cx_cy = perfect_imaging(
+        ch_sup,
+        ch_dir,
+        aperture_radius=fact_aperture_radius)
+    inside_pixels, ch_CHIDs = assign_to_pixels(
+        photons_cx_cy,
+        create_fact_pixel_tree())
+    ch_CHIDs = ch_CHIDs[inside_pixels]
+    arrival_times = arrival_times_for_cherenkov_photons_from_muon(
+        number_photons=ch_CHIDs.shape[0],
+        arrival_time_std=arrival_time_std)
+    arrival_times += 22e-9
+    phs = ps.PhotonStream()
+    phs.slice_duration = np.float32(
+        ps.io.magic_constants.TIME_SLICE_DURATION_S)
+    phs.raw = create_raw_photon_stream(
+        ch_CHIDs,
+        arrival_times,
+        nsb_rate_per_pixel)
+    event = ps.Event()
+    event.photon_stream = phs
+    event.zd = np.float32(0.0)
+    event.az = np.float32(0.0)
+    sim = ps.simulation_truth.SimulationTruth()
+    sim.reuse = np.uint32(0)
+    sim.run = np.uint32(0)
+    sim.event = np.uint32(event_id)
+    event.simulation_truth = sim
+    return event
---
+++ muons/muon_ring_simulation/test_ring_simulation.py
@@ -0,0 +1,232 @@
+import numpy as np
+import ring_simulation as rs
+import photon_stream as ps
+from scipy import stats
+
+np.random.seed(seed=1)
+
+
+def test_do_not_raise_exception():
+    photon_pos, photon_dir = rs.emit_photons(
+        np.array([0, 0, 1000]),
+        np.array([0, 0, -1]),
+        np.deg2rad(1.2),
+        ch_rate=1
+    )
+    xys = rs.draw_ch_photon_on_ground(photon_pos, photon_dir)
+    np.testing.assert_almost_equal(xys[:, 2], 0, 3)
+
+
+def test_simple_photons():
+    photon_pos = np.array([
+        [0, 0, 100],
+        [1, 0, 100],
+        [0, 1, 100],
+        [0, 2, 150]])
+    photon_dir = np.array([
+        [0, 0, -1],
+        [0, 0, -1],
+        [0, 0, -1],
+        [0, 0, -1]])
+    xys = rs.draw_ch_photon_on_ground(photon_pos, photon_dir)
+    assert xys.shape == (4, 3)
+    np.testing.assert_array_almost_equal(xys[0, :], [0, 0, 0], decimal=6)
+    np.testing.assert_array_almost_equal(xys[1, :], [1, 0, 0], decimal=6)
+    np.testing.assert_array_almost_equal(xys[2, :], [0, 1, 0], decimal=6)
+    np.testing.assert_array_almost_equal(xys[3, :], [0, 2, 0], decimal=6)
+
+
+def test_pol2cart1():
+    np.testing.assert_almost_equal(
+        rs.pol2cart(r=1, azimuth=0, inclination=0),
+        (0, 0, 1),
+        16
+    )
+
+
+def test_pol2cart2():
+    np.testing.assert_almost_equal(
+        rs.pol2cart(r=1, azimuth=0.5*np.pi, inclination=0.5*np.pi),
+        (0, 1, 0),
+        16
+    )
+
+
+def test_pol2cart3():
+    np.testing.assert_almost_equal(
+        rs.pol2cart(r=1, azimuth=0, inclination=0.5*np.pi),
+        (1, 0, 0),
+        16
+    )
+
+
+def test_emit_photons():
+    np.testing.assert_almost_equal(
+        len(
+            rs.emit_photons(
+                muon_support=[0, 1.5, 3000],
+                muon_direction=[0, 0, -1],
+                opening_angle=np.deg2rad(1.2),
+                ch_rate=3)[0]
+        ),
+        9000,
+        decimal=-2
+    )
+
+
+def test_draw_direction_for_cherenkov_photon1():
+    x = rs.draw_direction_of_cherenkov_photon(
+            opening_angle=np.deg2rad(1.2),
+            u=np.array([1, 0, 0]),
+            v=np.array([0, 1, 0]),
+            muon_direction=np.array([0, 0, -1])
+    )
+    np.testing.assert_almost_equal(
+        np.arccos(np.dot(x, np.array([0, 0, -1]))),
+        np.deg2rad(1.2)
+    )
+
+
+def test_draw_direction_for_cherenkov_photon2():
+    np.testing.assert_almost_equal(
+        rs.draw_direction_of_cherenkov_photon(
+            opening_angle=np.deg2rad(0.01),
+            u=np.array([1, 0, 0]),
+            v=np.array([0, 1, 0]),
+            muon_direction=np.array([0, 0, -1])
+        ),
+        [0, 0, -1],
+        3
+    )
+
+
+def test_get_u_v1():
+    np.testing.assert_equal(
+        np.dot(
+            np.array(rs.get_u_v(direction=[0, 0, -1]))[0],
+            [0, 0, -1]
+        ),
+        0
+    )
+
+
+def test_get_u_v2():
+    np.testing.assert_equal(
+        np.dot(
+            np.array(rs.get_u_v([0, 0, -1]))[1],
+            [0, 0, -1]
+        ),
+        0
+    )
+
+
+def test_get_u_v3():
+    np.testing.assert_equal(
+        np.dot(
+            np.array(rs.get_u_v([0, 0, -1]))[0],
+            np.array(rs.get_u_v([0, 0, -1]))[1]
+        ),
+        0
+    )
+
+
+def test_casaul_trajectory1():
+    np.testing.assert_equal(
+        rs.casual_trajectory(
+            support_vector=np.array([0, 0, 0]),
+            direction_vector=np.array([0, 0, 1])
+        )[1],
+        [0, 0, -1]
+    )
+
+
+def test_casaul_trajectory2():
+    np.testing.assert_equal(
+        rs.casual_trajectory(
+            np.array([0, 0, 0]),
+            np.array([0, 0, 1])
+        )[0],
+        [0, 0, 1000]
+    )
+
+
+def test_position_on_ray():
+    np.testing.assert_equal(
+        rs.position_on_ray(
+            support=np.array([0, 0, 0]),
+            direction=np.array([0, 0, 1]),
+            alpha=1000),
+        [0, 0, 1000]
+    )
+
+
+def test_draw_ch_photon_on_ground():
+    np.testing.assert_equal(
+        rs.draw_ch_photon_on_ground(
+            photon_emission_positions=np.array([
+                np.array([0, 0, 1000]),
+                np.array([0, 0, 0])
+            ]),
+            photon_directions=np.array([
+                np.array([0, 0, -1]),
+                np.array([-1, -1, -1])
+            ])
+        ),
+        np.array([np.array([0, 0, 0]), np.array([0, 0, 0])])
+    )
+
+
+def test_mask_for_photons_in_aperture_radius():
+    np.testing.assert_equal(
+        rs.mask_for_photons_in_aperture_radius(
+            intersection_point_on_xy_plane=np.array(
+                [[0, 0, 0], [0, 4, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]]
+            ),
+            aperture_radius=3
+        ),
+        np.array([True, False, True, True, True])
+    )
+
+
+def test_perfect_imaging():
+    np.testing.assert_equal(
+        rs.perfect_imaging(
+            photon_emission_positions=np.array(
+                [
+                    [0, 0, 1000],
+                    [0, 0, 0],
+                    [0, 0, 1],
+                    [0, 0, 1],
+                    [0, 0, 1]
+                ]
+            ),
+            photon_directions=np.array(
+                [
+                    [0, 0, -1],
+                    [1000, -1, -1],
+                    [1, 0, -1],
+                    [0, 1, -1],
+                    [5, 4, -1]
+                ]
+            )
+        ),
+        np.array(
+            [[0, 0], [1000, -1], [1, 0], [0, 1]]
+        )
+    )
+
+
+def test_create_raw_photon_stream():
+    nsb_rate_per_pixel = 35e6
+    raw = rs.create_raw_photon_stream(
+        pixel_CHIDs=np.array([]),
+        arrival_times=np.array([]),
+        nsb_rate_per_pixel=nsb_rate_per_pixel)
+    nr = raw != ps.io.binary.LINEBREAK
+    number_photons = np.sum(nr)
+    expected_photon_number = 1440*nsb_rate_per_pixel*50e-9
+    np.testing.assert_almost_equal(
+        number_photons,
+        expected_photon_number,
+        -2
+    )
