1d266f08c4a59d52aff9e6a914d7591375c33385
Laurits7 <laurits.tani@gmail.com>
2019-01-26 17:08:52 +0100

    real observation analysis

--- muons/analysis_utensils/real_observation_analysis/scoop_real_distributions.py
+++ muons/analysis_utensils/real_observation_analysis/scoop_real_distributions.py
@@ -2,82 +2,33 @@
 Distribution of muon ring features
 Call with 'python -m scoop --hostfile scoop_hosts.txt'
 
-Usage: scoop_real_distributions.py --muon_dir=DIR --output_dir=DIR
+Usage: scoop_real_distributions.py --muon_dir=DIR --output_dir=DIR --epochFile=PTH --scoop_hosts=PTH
 
 Options:
     --muon_dir=DIR      The location of muon data
     --output_dir=DIR    The output of caluculated fuzzyness
+    --epochFile=PTH     Path to the epoch file
+    --scoop_hosts=PTH   Path to scoop_hosts.txt
 """
 import photon_stream as ps
 import os
-import glob
-import matplotlib.pyplot as plt
-from muons.detection import detection
-from muons.detection_with_simple_ring_fit import (
-    detection_with_simple_ring_fit as ringM_detection)
-import numpy as np
 import docopt
-import fact
-import muons.muon_ring_fuzzyness.ring_fuzziness_with_amplitude as rfwa
-import muons.muon_ring_fuzzyness.muon_ring_fuzzyness as mrf
+import sys
+filePath = os.path.normpath(os.path.abspath(__file__))
+fileDir = os.path.normpath(os.path.join(filePath, os.pardir))
+parentDir = os.path.normpath(os.path.join(
+    fileDir, os.pardir))
+scriptDir = os.path.join(parentDir, "real_observation_analysis")
+sys.path.insert(0, scriptDir)
+import real_observation_analysis as roa
+import scoop
 
 
-def create_jobs(muon_dir, output_dir):
-    jobs = []
-    wild_card_path = os.path.join(muon_dir, "*", "*", "*", "*.phs.jsonl.gz")
-    for inpath in glob.glob(wild_card_path):
-        job = {}
-        job["inpath"] = inpath
-        job["output_dir"] = output_dir
-        jobs.append(job)
-    return jobs
-
-
-def run_job(job):
-    results = []
-    inpath = job["inpath"]
-    output_dir = job["output_dir"]
-    run = ps.EventListReader(inpath)
-    muon_ring_featuresR = []
-    muon_ring_featuresH = []
-    for event_id, event in enumerate(run):
-        photon_clusters = ps.PhotonStreamCluster(event.photon_stream)
-        muon_propsR = ringM_detection(event, photon_clusters)
-        muon_propsH = detection(event, photon_clusters)
-        if muon_propsH["is_muon"]:
-            muon_ring_featureH = [
-                muon_propsH["muon_ring_cx"],
-                muon_propsH["muon_ring_cy"],
-                muon_propsH["muon_ring_r"]
-            ]
-            muon_ring_featuresH.append(muon_ring_feature)
-        if muon_propsR["is_muon"]:
-            muon_ring_featureH = [
-                muon_propsR["muon_ring_cx"],
-                muon_propsR["muon_ring_cy"],
-                muon_propsR["muon_ring_r"]
-            ]
-            muon_ring_featuresR.append(muon_ring_feature)
-    fact_path = fact.path.parse(inpath)
-    night = fact_path["night"]
-    run = fact_path["run"]
-    filename = str(night) + "_" + str(run) + ".csv"
-    save_to_file("ringM", filename, muon_ring_featuresR)
-    save_to_file("hough", filename, muon_ring_featuresH)
-    return muon_ring_featuresR, muon_ring_featuresH
-
-
-def save_to_file(method, filename, muon_ring_features):
-    outpath = os.path.join(output_dir, method, filename)
-    header = list(["cx", "cy", "r"])
-    headers = ",".join(header)
-    np.savetxt(
-        outpath,
-        muon_ring_features,
-        delimiter=",",
-        comments="",
-        header=headers
-        )
+def main(muon_dir, output_dir, scoop_hosts, epochFile):
+    Analysis = roa.RealObservationAnalysis(
+        muon_dir, output_dir, epochFile, scoop_hosts)
+    jobs = Analysis.create_jobs()
+    job_return_codes = list(scoop.futures.map(Analysis.run_job, jobs))
 
 
 if __name__=='__main__':
@@ -85,9 +36,10 @@ if __name__=='__main__':
         arguments = docopt.docopt(__doc__)
         muon_dir = arguments['--muon_dir']
         output_dir = arguments['--output_dir']
+        scoop_hosts = arguments['--scoop_hosts']
+        epochFile = arguments['--epochFile']
         if not os.path.isdir(output_dir):
             os.makedirs(output_dir)
-        jobs = create_jobs(muon_dir, output_dir)
-        job_return_codes = list(scoop.futures.map(run_job, jobs))
+        main(muon_dir, output_dir, scoop_hosts, epochFile)
     except docopt.DocoptExit as e:
         print(e)
