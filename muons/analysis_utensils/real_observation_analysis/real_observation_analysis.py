d6df22c9fa6158ea192e025da0f578f3e83e3be2
Laurits7 <laurits.tani@gmail.com>
2019-02-11 15:03:43 +0100

    fix

--- muons/analysis_utensils/real_observation_analysis/real_observation_analysis.py
+++ muons/analysis_utensils/real_observation_analysis/real_observation_analysis.py
@@ -413,7 +413,7 @@ class RealObservationAnalysis:
         cys = []
         for run in glob.glob(wild_card_path):
             df = pandas.read_csv(run)
-            priont(df)
+            print(df)
             r = np.rad2deg(df["cx"])
             cx = np.rad2deg(df["cy"])
             cy = np.rad2deg(df["r"])
