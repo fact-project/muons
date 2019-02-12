a73b0f1a1f3fae90eabb2ef096ad37015f4436b8
Laurits7 <laurits.tani@gmail.com>
2019-02-12 10:22:31 +0100

    possible fix

--- muons/analysis_utensils/real_observation_analysis/real_observation_analysis.py
+++ muons/analysis_utensils/real_observation_analysis/real_observation_analysis.py
@@ -415,13 +415,15 @@ class RealObservationAnalysis:
         cys = []
         for run in glob.glob(wild_card_path):
             df = pandas.read_csv(run)
-            print(run)
-            r = np.rad2deg(df["cx"])
-            cx = np.rad2deg(df["cy"])
-            cy = np.rad2deg(df["r"])
-            rs.extend(r)
-            cxs.extend(cx)
-            cys.extend(cy)
+            try:
+                r = np.rad2deg(df["cx"])
+                cx = np.rad2deg(df["cy"])
+                cy = np.rad2deg(df["r"])
+            except AttributeError:
+                continue
+                rs.extend(r)
+                cxs.extend(cx)
+                cys.extend(cy)
         return cxs, cys, rs
 
 
